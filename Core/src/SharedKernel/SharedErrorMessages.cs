8460502b4b2f36315daabddb4ff0360dc2166c52
tarekmansour <tarekmansour.net@gmail.com>
2024-11-30 16:12:37 +0100

    organize layers

---
+++ Core/src/SharedKernel/SharedErrorMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedKernel;
+public static class SharedErrorMessages
+{
+    public static readonly string SuccessfulResultCannotHaveError = "A successful result cannot have an error.";
+    public static readonly string SuccessfulResultCannotHaveErrors = "A successful result cannot have an errors.";
+    public static readonly string SuccessfulResultMustHaveValue = "A successful result must have a value.";
+
+    public static readonly string FailedResultMustHaveError = "A failed result must have an error.";
+    public static readonly string FailedResultMustHaveErrors = "A failed result must have at least one error.";
+}
