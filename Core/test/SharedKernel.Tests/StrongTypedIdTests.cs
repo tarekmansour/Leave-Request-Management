965247eeacf38e24aed93e71ce7b5305b1b4fec0
tarekmansour <tarekmansour.net@gmail.com>
2024-11-30 23:50:48 +0100

    add StrongTypedIdTests

---
+++ Core/test/SharedKernel.Tests/StrongTypedIdTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace SharedKernel.Tests;
+public class StrongTypedIdTests
+{
+    [Fact(DisplayName = "Should create StrongTypedId with valid positive value")]
+    public void New_ShouldCreateStrongTypedId_WhenValueIsGreaterThanZero()
+    {
+        // Arrange
+        int validValue = 1;
+
+        // Act
+        var strongTypedId = new TestStrongTypedId(validValue);
+
+        // Assert
+        strongTypedId.Should().NotBeNull();
+        strongTypedId.Should().NotBeSameAs(validValue);
+    }
+
+    [Fact(DisplayName = "Should throw ArgumentException when value is zero")]
+    public void New_ShouldThrowArgumentException_WhenValueIsZero()
+    {
+        // Arrange
+        int invalidValue = 0;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new TestStrongTypedId(invalidValue));
+    }
+
+    private record TestStrongTypedId : StrongTypedId
+    {
+        public TestStrongTypedId(int value) : base(value) { }
+    }
+}
