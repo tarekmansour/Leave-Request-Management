22a3c2553f6f42c5c9078b5e278e1307ac0b0524
tarekmansour <tarekmansour.net@gmail.com>
2024-12-02 23:49:38 +0100

    add tests

---
+++ Core/test/Application.Tests/Commands/Login/LoginUserCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Commands.UserLogin;
+using FluentAssertions;
+
+namespace Application.Tests.Commands.Login;
+public partial class LoginUserCommandTests
+{
+    [Fact(DisplayName = "new LoginUserCommandHandler with null repository")]
+    public void WithNullRepository_Should_ThrowException()
+    {
+        // Arrange & act
+        var act = () => new LoginUserCommandHandler(
+            _logger,
+            _commandValidator,
+            null!,
+            _passwordHasher,
+            _tokenProvider);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "new LoginUserCommandHandler with null validator")]
+    public void WithNullValidator_Should_ThrowException()
+    {
+        // Arrange & act
+        var act = () => new LoginUserCommandHandler(
+            _logger,
+            null!,
+            _userRepository,
+            _passwordHasher,
+            _tokenProvider);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
---
+++ Core/test/Application.Tests/Commands/Login/LoginUserCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Abstractions;
+using Application.Commands.UserLogin;
+using Domain.Repositories;
+using FluentValidation;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using SharedKernel.Tests;
+
+namespace Application.Tests.Commands.Login;
+
+[ExcludeFromCodeCoverage]
+public partial class LoginUserCommandTests : DatabaseFixture
+{
+    private readonly ILogger<LoginUserCommandHandler> _logger;
+    private readonly IValidator<LoginUserCommand> _commandValidator;
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly ITokenProvider _tokenProvider;
+    private readonly LoginUserCommandHandler _sut;
+
+    public LoginUserCommandTests()
+    {
+        _logger = Substitute.For<ILogger<LoginUserCommandHandler>>();
+        _commandValidator = new LoginUserCommandValidator();
+        _userRepository = new UserRepository(_dbContext);
+        _passwordHasher = Substitute.For<IPasswordHasher>();
+        _tokenProvider = Substitute.For<ITokenProvider>();
+        _sut = new LoginUserCommandHandler(_logger, _commandValidator, _userRepository, _passwordHasher, _tokenProvider);
+    }
+}
